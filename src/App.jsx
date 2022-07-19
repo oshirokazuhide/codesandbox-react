8b490fa027b4a74cf66438dbe426f2888a61fd1d
oshirokazuhide <89958891+oshirokazuhide@users.noreply.github.com>
2022-07-19 11:07:11 +0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+
+  return (
+    <>
+      <h1 style={{ color: "red", fontStyle: "italic" }}>Hello, react</h1>
+      <ColorfulMessage color="blue">How are you?</ColorfulMessage>
+      <ColorfulMessage color="pink">Fine!</ColorfulMessage>
+
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => {
-  return <h1>Hello, react</h1>;
-};
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hello, react</h1>
+//       <p>How are you?</p>
+//     </>
+//   );
+// };
 
 ReactDom.render(<App />, document.getElementById("root"));
