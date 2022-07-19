8767c04b762cb56bee5148c4017aeffc576697ca
oshirokazuhide <89958891+oshirokazuhide@users.noreply.github.com>
2022-07-19 11:36:02 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-
+  console.log("first");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+  const onClickSwichShoeFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
   return (
     <>
       <h1 style={{ color: "red", fontStyle: "italic" }}>Hello, react</h1>
       <ColorfulMessage color="blue">How are you?</ColorfulMessage>
       <ColorfulMessage color="pink">Fine!</ColorfulMessage>
 
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>count up!</button>
+      <br />
+      <button onClick={onClickSwichShoeFlag}>on/off</button>
+      <p>{num}</p>
+
+      {faceShowFlag && <p>!(^^)!</p>}
     </>
   );
 };
