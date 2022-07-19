8627e04e1aad18be6ab0f2a9fcbe23bb4b95bbe6
oshirokazuhide <89958891+oshirokazuhide@users.noreply.github.com>
2022-07-19 10:33:01 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return <h1>Hello, react</h1>;
+};
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
